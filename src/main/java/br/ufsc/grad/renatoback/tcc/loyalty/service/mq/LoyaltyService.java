5b396b5c09a778820f253af47530e8d34a1e93d2
renato <renato@knewin.com>
2016-11-19 18:36:25 -0200

    Limpeza.

--- src/main/java/br/ufsc/grad/renatoback/tcc/loyalty/service/mq/LoyaltyService.java
+++ src/main/java/br/ufsc/grad/renatoback/tcc/loyalty/service/mq/LoyaltyService.java
@@ -1,21 +1,12 @@
 package br.ufsc.grad.renatoback.tcc.loyalty.service.mq;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LoyaltyService {
 
-	private Log logger = LogFactory.getLog(LoyaltyService.class);
-
-	AtomicInteger counter = new AtomicInteger();
-
-	public void createPointsBalance(String time) {
-		logger.info(String.format("OK Loyalty #%d after %d ns", counter.incrementAndGet(),
-				System.nanoTime() - Long.parseLong(time)));
+	public void createPointsBalance(String userId) {
+		System.err.println(".");
 	}
 
 }
